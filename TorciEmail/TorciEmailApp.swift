3be5f60a9c4318b68ee0dea213f0c229c6b9fd88
torcy-it <torcy.ado@gmail.com>
2025-11-16 08:50:38 +0100

    Initial Commit

---
+++ TorciEmail/TorciEmailApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorciEmailApp.swift
+//  TorciEmail
+//
+//  Created by Adolfo Torcicollo on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TorciEmailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
